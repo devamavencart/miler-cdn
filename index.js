ffd520e3b436ada8b66af4b8d7c984bc031df93f
devamavencart <103481945+devamavencart@users.noreply.github.com>
2023-08-02 09:54:22 +0530

    Update index.js
    change xmlhttp to fetch method

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 // Wait for the DOM to be fully loaded before attaching event listeners
-document.addEventListener('DOMContentLoaded', function () {
-    // Get a reference to the button element you want to capture the click event for
-    const myButton = document.getElementById('my-button');
-
-    // Attach a click event listener to the button
-    myButton.addEventListener('click', function (event) {
-        // This function will be executed when the button is clicked
-        // You can add your event handling logic here
-        console.log('--start--Button clicked!', event);
-
-            // Create a new XMLHttpRequest object
-            var xhr = new XMLHttpRequest();
-
-            // Configure the request
-            xhr.open('POST', 'http://127.0.0.1:3032/miler_cdn', true);
-            xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    // Request was successful, and response is available in xhr.responseText
-                    console.log("response", xhr.response); 
-                }
-                console.log("fetched successfully", xhr.status); 
-                }
-            };
-
-            // Send the request
-            xhr.send(event);
-        
-        console.log('---end--Button clicked!');
-    });
+document.addEventListener('click', function (event) {
+        const screenInfo = {
+            width: window.screen.width,
+            height: window.screen.height,
+        };
+        const eventObject = { 
+            type: event.target.tagName,
+            screenInfo: screenInfo,
+            timestamp: new Date(8.64e15).toString(),
+        };
+          // Make an API call to the backend to send the event data
+        fetch('http://127.0.0.1:3032/miler_cdn/event', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body:JSON.stringify(eventObject),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Response from backend:', data);
+        })
+        .catch((error) => {
+            console.error('Error sending event:', error);
+        });
 });
