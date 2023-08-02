ae3463ad225f11f05c1c6d74ae1e4d348c70a3af
devamavencart <103481945+devamavencart@users.noreply.github.com>
2023-08-02 12:53:10 +0530

    Update index.js
    changeUrl

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ document.addEventListener('click', function (event) {
             timestamp: new Date(8.64e15).toString(),
         };
           // Make an API call to the backend to send the event data
-        fetch('http://127.0.0.1:3032/miler_cdn/event', {
+        fetch('https://773d-49-205-87-45.ngrok-free.app/miler_cdn/event', {
             method: 'POST',
             headers: {
             'Content-Type': 'application/json',
